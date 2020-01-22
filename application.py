6f53059969096ca0bd6791cf6355e5eda09b7e3b
glitchymoons <glitchymoons@gmail.com>
2020-01-22 23:51:45 +0000

    modified application.py

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import caesar
 import vigenere
@@ -165,3 +166,8 @@ def scytale_dec():
 
     # Output ciphertext
     return render_template("scytale.html", inptD=inptD, keyD=keyD, outptD=outptD)
+
+if __name__ == ‘__main__’:
+ app.debug = True
+ port = int(os.environ.get(“PORT”, 5000))
+ app.run(host=’0.0.0.0', port=port)
