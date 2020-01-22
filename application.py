4d2eaf983d471392f43afd403f3dd6e6c2f5dc86
glitchymoons <glitchymoons@gmail.com>
2020-01-22 23:56:06 +0000

    modified application.py

--- application.py
+++ application.py
@@ -167,7 +167,7 @@ def scytale_dec():
     # Output ciphertext
     return render_template("scytale.html", inptD=inptD, keyD=keyD, outptD=outptD)
 
-if __name__ == ‘__main__’:
+if __name__ == "__main__":
  app.debug = True
- port = int(os.environ.get(“PORT”, 5000))
- app.run(host=’0.0.0.0', port=port)
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host='0.0.0.0', port=port)
